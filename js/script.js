4fc2748e32c23fa215949ac762a3518b543be3c7
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-08-12 23:56:59 +0500

    jd yuklandi

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const darkbtn = document.getElementById('darkbtn');
+const body = document.body;
+
+const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+if (isDarkMode) {
+    body.classList.add('dark-mode');
+    darkbtn.checked = true;
+}
+
+darkbtn.addEventListener('change', () => {
+    if (darkbtn.checked) {
+        body.classList.add('dark-mode');
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
